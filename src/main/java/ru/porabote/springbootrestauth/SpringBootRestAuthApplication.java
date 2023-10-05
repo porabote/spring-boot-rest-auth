9712e5493c69c6e7a8b857f3b8ab0d139bf4b466
dmaksimov <dmaksimov@vtb.ru>
2023-10-05 20:22:05 +0300

    auth by method

--- src/main/java/ru/porabote/springbootrestauth/SpringBootRestAuthApplication.java
+++ src/main/java/ru/porabote/springbootrestauth/SpringBootRestAuthApplication.java
@@ -2,8 +2,12 @@ package ru.porabote.springbootrestauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @SpringBootApplication
@@ -14,18 +18,33 @@ public class SpringBootRestAuthApplication {
     }
 
     @GetMapping("/hi")
+    @Secured({"ROLE_ADMIN"})
     public String hi() {
         return "Hi";
     }
 
     @GetMapping("/bye")
+    @Secured({"READ"})
     public String bye() {
         return "bye";
     }
 
     @GetMapping("/write")
+    @RolesAllowed({"WRITE"})
     public String write() {
         return "write";
+    }
+
+    @GetMapping("/update")
+    @PreAuthorize("hasAuthority('WRITE') or hasAuthority('DELETE')")
+    public String update() {
+        return "update";
+    }
+
+    @GetMapping("/profile")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String profile(String username) {
+        return "It profile " + username;
     }
 
 }
--- src/main/java/ru/porabote/springbootrestauth/security/SecurityConfiguration.java
+++ src/main/java/ru/porabote/springbootrestauth/security/SecurityConfiguration.java
@@ -2,10 +2,12 @@ package ru.porabote.springbootrestauth.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -22,7 +24,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Denis").password("pas123").authorities("write");
+                .withUser("Denis").password("pas123").roles("READ")
+                .and().withUser("Petya").password("pas1235").roles("WRITE")
+                .and().withUser("Leva").password("pas1236").roles("DELETE");//.authorities("bye");
     }
 
 }
