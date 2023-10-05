2c789235033b974886a816c8bad7ba5dbcc8720f
dmaksimov <dmaksimov@vtb.ru>
2023-10-05 15:12:05 +0300

    security

---
+++ src/main/java/ru/porabote/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package ru.porabote;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and()
+                .authorizeHttpRequests().antMatchers("/write").hasAuthority("write")
+                .and()
+                .authorizeHttpRequests().antMatchers("/hi").permittAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Denis").password("pas123").authorities("write");
+    }
+
+}
--- src/main/java/ru/porabote/springbootrestauth/SpringBootRestAuthApplication.java
+++ src/main/java/ru/porabote/springbootrestauth/SpringBootRestAuthApplication.java
@@ -2,12 +2,30 @@ package ru.porabote.springbootrestauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootRestAuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRestAuthApplication.class, args);
+    }
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "Hi";
+    }
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "bye";
+    }
+
+    @GetMapping("/write")
+    public String write() {
+        return "write";
     }
 
 }
